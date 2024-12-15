79a8220ee4bdfc114b48b41743df943d1d660aef
Hamza Krika <hamza.krika@etu.unistra.fr>
2024-12-15 23:50:34 +0100

    home, login and signup pages setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 
+import HomePage from './pages/home/HomePage'
+import LoginPage from './pages/auth/login/LoginPage'
+import SignupPage from './pages/auth/signup/SignUpPage'
+
 function App() {
 
   return (
-    <div className="flex max-w-6xl mx-auto">
+    <div className="flex mx-auto m-0 p-0">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,4 @@
+const LoginPage = () => {
+  return <div>home Page</div>
+}
+export default LoginPage;
