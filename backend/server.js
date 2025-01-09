4f171b131a70388b2abff79516f4807266db1406
Hamza Krika <hamza.krika@etu.unistra.fr>
2025-01-09 15:04:07 +0100

    config changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ import leaderboardRoutes from "./routes/leaderboard.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
+import path from "path";
+
 dotenv.config();
 
 cloudinary.config({
@@ -21,6 +23,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json( {limit:"5mb"} ));
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -32,6 +35,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/routes", routeRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
