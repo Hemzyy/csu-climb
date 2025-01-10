6ade3d20ac4dfa7d3fb198305a7cfd6015b6d78f
Hamza Krika <hamza.krika@etu.unistra.fr>
2025-01-10 17:32:53 +0100

    ready for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlifyPlugin()],
   server: {
     port: 3000,
     proxy: {
