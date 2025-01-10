980779b99806e67ef7efaeeb10aedb1d6bede75e
Hamza Krika <hamza.krika@etu.unistra.fr>
2025-01-10 17:53:40 +0100

    build error fix 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import netlifyVitePluginReactRouter from '@netlify/vite-plugin-react-router';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlifyPlugin()],
+  plugins: [react(), netlifyPlugin(), netlifyVitePluginReactRouter],
   server: {
     port: 3000,
     proxy: {
